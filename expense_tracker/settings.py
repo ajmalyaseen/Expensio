eb8ca642c50f86dde3949d0b4c80f752288764db
Yaa.si_ <83084169+ajmalyaseen@users.noreply.github.com>
2025-12-07 21:13:10 +0530

    Prepared for deployment

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-t#1@sj-wy7%npg*d1p1g*ks45@$c8^d5*-vktjh-9^lte#_(w4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,6 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGIN_REDIRECT_URL = 'index'
 
 LOGOUT_REDIRECT_URL = 'login'
